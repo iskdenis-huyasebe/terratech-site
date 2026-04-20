bd102a32c344a075ec1e55d9af7f3af7c664900a
iskdenis-huyasebe <iskdenis@gmail.com>
2026-04-20 19:12:45 +0300

    fix: next.config types

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'static.wixstatic.com' }
+      { protocol: 'https' as const, hostname: 'static.wixstatic.com' }
     ]
   }
 };
